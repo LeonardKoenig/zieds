2411823fd9aa57cce4e9d99d784f459bb23fff14
Leonard König <leonard.r.koenig@googlemail.com>
2016-08-27 14:35:47 +0200

    Add build system and example code
     - CMakeLists.txt for CMake building
      - use `mkdir build && cd build && cmake .. && make` to build
     - src/example.c is some code to test functionality and provide an
    example on how to use this (to-come) library

---
+++ src/example.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+#include <bloom_filter.h>
+
+int main()
+{
+	struct bloom_filter *bf;
+	BLOOM_INIT(bf);
+
+	int32_t data[] = { 42, 4, 8, 7, 3, 2 };
+	size_t len = sizeof data / sizeof data[0];
+	printf("set: ");
+	for (size_t i = 0; i < len; i++) {
+		bloom_insert_int(bf, data[i]);
+		printf("%c%" PRId32 "%c",
+				(i == 0) ? '[' : ' ',
+				data[i],
+				(i+1 >= len) ? ']' : ',');
+	}
+	printf("\n");
+
+	int32_t data2[] = { 42, 7, 6, 1, 0, -4, 8 };
+	size_t len2 = sizeof data2 / sizeof data2[0];
+	for (size_t i = 0; i < len; i++) {
+		printf("% 3" PRId32 " in set?: %s\n",
+				data2[i],
+				bloom_query_int(bf, data2[i]) ? "true" : "false" );
+	}
+
+	BLOOM_FREE(bf);
+
+}
